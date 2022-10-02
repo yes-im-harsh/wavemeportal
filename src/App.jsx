93e1d51803bbb17154352c1732f773146ba8e4d9
Harsh Chauhan <76259017+yes-im-harsh@users.noreply.github.com>
2022-10-02 06:18:32 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+      
+      
+    
+
+      <div className="dataContainer">
+        <div className="title"><h1>Wave Me Portal</h1></div>
+        
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me 👋
+        </button>
+      </div>
+    </div>
+  );
+}
